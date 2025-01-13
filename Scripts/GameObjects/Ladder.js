e3ce6bce4ce621ef70170381ee348438e0da93d0
iqqx <boknok@list.ru>
2025-01-13 21:14:03 +0300

    Лестницы пофикшены...

--- Scripts/GameObjects/Ladder.js
+++ Scripts/GameObjects/Ladder.js
@@ -1,6 +1,4 @@
-import { Scene } from "../Scene.js";
-import { Canvas } from "../Context.js";
-import { Color, Rectangle } from "../Utilites.js";
+import { Rectangle } from "../Utilites.js";
 import { Tag } from "../Enums.js";
 import { GameObject } from "./GameObject.js";
 export class Ladder extends GameObject {
@@ -10,10 +8,6 @@ export class Ladder extends GameObject {
         this._x = x;
         this._y = y;
         this._collider = new Rectangle(0, 0, 50, height);
-    }
-    Render() {
-        Canvas.SetFillColor(new Color(100, 50, 50, 100));
-        Canvas.DrawRectangle(this._x - Scene.Current.GetLevelPosition(), this._y, this.Width, this.Height);
     }
 }
 //# sourceMappingURL=Ladder.js.map
--- src/GameObjects/Ladder.ts
+++ src/GameObjects/Ladder.ts
@@ -1,6 +1,4 @@
-import { Scene } from "../Scene.js";
-import { Canvas } from "../Context.js";
-import { Color, Rectangle } from "../Utilites.js";
+import { Rectangle } from "../Utilites.js";
 import { Tag } from "../Enums.js";
 import { GameObject } from "./GameObject.js";
 
@@ -12,10 +10,5 @@ export class Ladder extends GameObject {
 		this._x = x;
 		this._y = y;
 		this._collider = new Rectangle(0, 0, 50, height);
-	}
-
-	override Render(): void {
-		Canvas.SetFillColor(new Color(100, 50, 50, 100));
-		Canvas.DrawRectangle(this._x - Scene.Current.GetLevelPosition(), this._y, this.Width, this.Height);
 	}
 }
